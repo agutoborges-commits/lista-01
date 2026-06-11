29d3e310e8840c3b5732c6a004301324c483b7ce
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-11 19:35:32 -0300

    q1

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Qual é sua idade?");
+int idade = int.Parse(Console.ReadLine());
+if (idade >= 16)
+{
+    Console.WriteLine("Entrada permitida");
+}
+else
+{
+    Console.WriteLine("Entrada somente com responsável");
+}
