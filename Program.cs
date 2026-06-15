9cd6987ba9da98fd245c97ba40f34bd1358788e0
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 19:50:41 -0300

    q11

--- Program.cs
+++ Program.cs
@@ -1,17 +1,6 @@
-﻿Console.WriteLine("Informe a idade do passageiro:");
-int idade = int.Parse(Console.ReadLine());
-Console.WriteLine("Informe o valor normal da passagem: ");
-double valorPassagem = double.Parse(Console.ReadLine());
-if (idade < 5)
+﻿Console.WriteLine("Informe o número final da contagem: ");
+int numeroFinal = int.Parse(Console.ReadLine());
+for (int i = 1; i <= numeroFinal; i++)
 {
-    Console.WriteLine("Passagem gratuita");
-}
-else if (idade >= 60)
-{
-    double valorDesconto = valorPassagem * 0.5;
-    Console.WriteLine($"Valor da passagem com desconto: {valorDesconto}");
-}
-else if (idade > 5 && idade < 60)
-{
-    Console.WriteLine($"Valor normal da passagem: {valorPassagem}");
+    Console.WriteLine(i);
 }
