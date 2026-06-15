0988de85516e94ac43d6fb17b44ecd314a000577
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 20:03:12 -0300

    q15

--- Program.cs
+++ Program.cs
@@ -1,14 +1,36 @@
-﻿Console.WriteLine("Informe o primeiro valor: ");
-int valor1 = Convert.ToInt32(Console.ReadLine());       
-Console.WriteLine("Informe o segundo valor: ");
-int valor2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Informe o terceiro valor: ");
-int valor3 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Informe o quarto valor: ");
-int valor4 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Informe o quinto valor: ");
-int valor5 = Convert.ToInt32(Console.ReadLine());
-double soma = valor1 + valor2 + valor3 + valor4 + valor5;
-Console.WriteLine("A soma dos valores é: " + soma);
-double media = (valor1 + valor2 + valor3 + valor4 + valor5) / 5.0;
-Console.WriteLine("A média dos valores é: " + media);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int positivos = 0;
+        int negativos = 0;
+        int zeros = 0;
+
+        // Repete 10 vezes
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write("Informe um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > 0)
+            {
+                positivos++;
+            }
+            else if (numero < 0)
+            {
+                negativos++;
+            }
+            else
+            {
+                zeros++;
+            }
+        }
+
+        Console.WriteLine("Quantidade de positivos: " + positivos);
+        Console.WriteLine("Quantidade de negativos: " + negativos);
+        Console.WriteLine("Quantidade de zeros: " + zeros);
+    }
+}
