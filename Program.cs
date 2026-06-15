ecad802cd26ec1ed1dc74855e5ecf00e023ae697
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 20:06:18 -0300

    q16

--- Program.cs
+++ Program.cs
@@ -1,36 +1,10 @@
-﻿using System;
-
-class Program
+﻿Console.WriteLine("Informe a senha: ");
+string senha = Console.ReadLine();
+if (senha == "1234")
 {
-    static void Main()
-    {
-        int numero;
-        int positivos = 0;
-        int negativos = 0;
-        int zeros = 0;
-
-        // Repete 10 vezes
-        for (int i = 1; i <= 10; i++)
-        {
-            Console.Write("Informe um número: ");
-            numero = int.Parse(Console.ReadLine());
-
-            if (numero > 0)
-            {
-                positivos++;
-            }
-            else if (numero < 0)
-            {
-                negativos++;
-            }
-            else
-            {
-                zeros++;
-            }
-        }
-
-        Console.WriteLine("Quantidade de positivos: " + positivos);
-        Console.WriteLine("Quantidade de negativos: " + negativos);
-        Console.WriteLine("Quantidade de zeros: " + zeros);
-    }
+    Console.WriteLine("Acesso permitido");
+}
+else
+{
+    Console.WriteLine("Acesso negado");
 }
