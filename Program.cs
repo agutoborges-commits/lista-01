bca050d1b3258d370b6054b77b2e4e0bec13968d
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 19:38:17 -0300

    q9

--- Program.cs
+++ Program.cs
@@ -1,18 +1,10 @@
-﻿Console.WriteLine("informe a primeira pontuação: ");
-int pontuacao1 = int.Parse(Console.ReadLine());
-Console.WriteLine("informe a segunda pontuação: ");
-int pontuacao2 = int.Parse(Console.ReadLine());
-Console.WriteLine("informe a terceira pontuação: ");
-int pontuacao3 = int.Parse(Console.ReadLine());
-if (pontuacao1 > pontuacao2 && pontuacao1 > pontuacao3)
+﻿Console.WriteLine("Informe um numero inteiro:");
+int numero = int.Parse(Console.ReadLine());
+if (numero % 2 == 0)
 {
-    Console.WriteLine("a maior pontuação é: " + pontuacao1);
-}
-else if (pontuacao2 > pontuacao1 && pontuacao2 > pontuacao3)
-{
-    Console.WriteLine("a maior pontuação é: " + pontuacao2);
+    Console.WriteLine("O numero é par");
 }
 else
 {
-    Console.WriteLine("a maior pontuação é: " + pontuacao3);
+    Console.WriteLine("O numero é impar");
 }
