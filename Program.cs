f2b21285c8efdc657007bf10968d37464fdf8fc2
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 19:53:23 -0300

    q12

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine("Informe o número final da contagem: ");
-int numeroFinal = int.Parse(Console.ReadLine());
-for (int i = 1; i <= numeroFinal; i++)
+﻿Console.WriteLine("Informe o número inicial da contagem regressiva:");
+int numeroInicial = int.Parse(Console.ReadLine());
+for (int i = numeroInicial; i >= 0; i--)
 {
     Console.WriteLine(i);
 }
