06389c5fce95f2975de20332c770aa5700294fdf
Augusto W borges <augusto_w_borges@estudante.sesisenai.org.br>
2026-06-15 22:20:13 -0300

    Calculadora

---
+++ Calculadora.cs
@@ -0,0 +1,19 @@
+public class Calculadora
+{
+    public int Somar(int a, int b)
+    {
+        return a + b;
+    }
+    public int multiplicar(int a, int b)
+    {
+        return a * b;
+    }
+    public double delta(double a, double b, double c)
+    {
+        return (b * b) - (4 * a * c);
+    }
+    public double bhaskara(double a, double b, double delta)
+    {
+        return (b * -1 + Math.Sqrt(delta)) / (2 * a);
+    }
+}
